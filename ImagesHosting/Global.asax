ffc6a719fc69b7a8a3a728aa8f35a0eade8d8be5
kastjan <kastjang@gmail.com>
2015-11-07 21:00:00 +0300

    ImagesHosting v1.0

---
+++ ImagesHosting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImagesHosting.MvcApplication" Language="C#" %>
---
+++ ImagesHosting/Models/user_request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+
+namespace ImagesHosting.Models
+{
+    public class user_request
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+    }
+}
